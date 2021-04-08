66c1a10da499029e21d400a831416b37ff4b9417
Vitthal Sai Kaul <68645683+Babapool@users.noreply.github.com>
2021-04-08 19:32:19 +0530

    Add files via upload
    Code to have a Pyramid Pattern using numbers

--- DQ_learn_cpp/main.cpp
+++ DQ_learn_cpp/main.cpp
@@ -7,18 +7,24 @@ int main()
     int i,j,n;
     cout<<"\n Enter number of rows:";
     cin>>n;
-    cout<<endl<<endl;
 
-    for(i=0;i<n;i++)
+    int a=1;
+
+    for(i=n;i>0;i--)
     {
-       for(j=0;j<n-i;j++)
+        for(j=1;j<=i;j++)
+        {
             cout<<" ";
-       for(j=0;j<n;j++)
-            cout<<"*";
+        }
 
-       cout<<"\n";
+        for(j=1;j<=a;j++)
+        {
+            cout<<j<<" ";
+        }
+
+        cout<<"\n";
+        a++;
     }
-
     return 0;
 }
 
@@ -27,14 +33,12 @@ OUTPUT:
 
 
  Enter number of rows:5
+     1
+    1 2
+   1 2 3
+  1 2 3 4
+ 1 2 3 4 5
 
-
-     *****
-    *****
-   *****
-  *****
- *****
-
-Process returned 0 (0x0)   execution time : 2.631 s
+Process returned 0 (0x0)   execution time : 2.577 s
 Press any key to continue.
 */
